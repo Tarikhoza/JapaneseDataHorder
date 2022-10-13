6ce7961c78176e3bc90c3256c77c83a5665405f7
Tarikhoza <tarik.246@hotmail.com>
2022-10-13 22:33:41 +0200

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='Japanese Data Horder',
     version='1.0',
     packages = find_packages(),
-    install_requires=install_requires[
+    install_requires=[
         "geckodriver-autoinstaller==0.1.0"
         "Pillow==9.2.0",
         "requests==2.28.1",
