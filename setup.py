a2e951c556fa0c026f46c11e91e4df002153ea44
Tarikhoza <tarik.246@hotmail.com>
2023-05-15 16:04:32 +0200

    fixed name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='Japanese Data Horder',
+    name='JapaneseDataHorder',
     version='1.0',
     packages = find_packages(),
     install_requires=[
